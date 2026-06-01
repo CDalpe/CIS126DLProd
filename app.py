73d3f55a817354f521025a771dcf1c8b7c3a13fe
CDalpe <dalpechristopher@gmail.com>
2026-06-01 08:13:14 -0700

    course yaml load
    also corrected two typos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_file, render_template, jsonify
+import markdown, yaml, os
 
 app = Flask (__name__)
 
@@ -6,7 +7,9 @@ app = Flask (__name__)
 def home():
     return render_template('/home.html')
 
+with app.app_context():
+    with open("course.yaml", 'r') as f:
+        course_data = yaml.safe_load(f)
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-test = True
