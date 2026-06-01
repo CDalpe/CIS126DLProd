04a2b494ad92de39b4f78375dbc34e6151947d4f
CDalpe <dalpechristopher@gmail.com>
2026-06-01 08:36:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, send_file, render_template
 import markdown, yaml, os
 
 app = Flask(__name__)
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def home():
     return render_template('/home.html')
 
+course_data = None
 with app.app_context():
-    with open("course.yaml", 'r') as f:
+    with open(os.path.join(base_dir, 'course.yaml'), 'r') as f:
         course_data = yaml.safe_load(f)
 
 if __name__ == "__main__":
