2ce255594857a8abe8402b4d4237f9b94746deba
CDalpe <dalpechristopher@gmail.com>
2026-05-31 20:53:02 -0700

    Init
    Init

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, send_file, render_template, jsonify
+
+app = Flask (__name__)
+
+@app.route("/")
+def home():
+    return render_template('/home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
